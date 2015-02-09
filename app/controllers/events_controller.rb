109feeffce135557d5a7f135d17eb0f53260aba0
Kenneth To <kennethkto@gmail.com>
2015-02-09 14:43:02 -0700

    fix routing issues, render forms and tables

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,46 @@
 class EventsController < ApplicationController
 
+  def index
+    @event = Event.all
+  end
+
   def new
+    @location = Location.find(params[:location_id])
     @event = Event.new
   end
 
   def show
     @event = Event.find(params[:id])
+    @location = Location.find(params[:location_id])
   end
 
   def edit
     @event = Event.find(params[:id])
+    @location = Location.find(params[:location_id])
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @location = Location.find(params[:location_id])
+    @event.destroy
+    redirect_to location_path(@location)
+  end
+
+  def create
+  @event = Event.new(event_params)
+  @event.location_id = params[:location_id]
+    if @event.save
+      redirect_to locations_path, notice: 'Event was successfully created.'
+    else
+      render:new
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @location = Location.find(params[:location_id])
+    @event.update_attributes(event_params)
+    redirect_to location_event_path(@location, @event), notice: "Event was sucessfully updated"
   end
 
 private
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,18 +1,34 @@
 class LocationsController < ApplicationController
   def index
-    location = Location.new
+    @location = Location.all
   end
 
   def new
-    @location = Location.find(params[:id])
+    @location = Location.new
   end
 
   def show
     @location = Location.find(params[:id])
+    
   end
 
   def edit
     @location = Location.find(params[:id])
+  end
+
+  def create
+  @location = Location.new(location_params)
+    if @location.save
+      redirect_to @location, notice: 'Location was successfully created.'
+    else
+      render:new
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+    redirect_to locations_path
   end
 
 private
