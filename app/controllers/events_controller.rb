a929434e23013627eed07825a82aa6c17a86576c
Kenneth To <kennethkto@gmail.com>
2015-02-09 11:20:50 -0700

    change root route to locations index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,25 @@
 class EventsController < ApplicationController
-  
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:description, :date, :requires_id)
+  end
+
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,3 +1,29 @@
 class LocationsController < ApplicationController
+  def index
+    location = Location.new
+  end
+
+  def new
+    @location = Location.find(params[:id])
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :address, :zipcode)
+  end
+
 
 end
